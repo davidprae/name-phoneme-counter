9066ef0ec3bef41e589f4438da14b9605074c235
dylanmartin <dylanrossmartin@gmail.com>
2020-05-09 20:58:29 -0400

    add popularity to names

--- index.mjs
+++ index.mjs
@@ -1,10 +1,13 @@
-import getBabyNames from './babynames.mjs';
-import voicedPhonemes from './phonemes.mjs';
+import getBabyNames from "./babynames.mjs";
+import voicedPhonemes from "./phonemes.mjs";
+import usbabynames from "us-baby-names";
 
 const babyNames = getBabyNames();
 
 function getBabyNamesWithVoicedPhonemesCount(babyNames) {
-  return babyNames.map(babyName => getBabyNameWithVoicedPhonemesCount(babyName));
+  return babyNames.map((babyName) =>
+    getBabyNameWithVoicedPhonemesCount(babyName)
+  );
 }
 
 function getBabyNameWithVoicedPhonemesCount(babyName) {
@@ -15,23 +18,37 @@ function getBabyNameWithVoicedPhonemesCount(babyName) {
   return {
     ...babyName,
     voicedPhonemeCount,
-    voicelessPhonemeCount
-  }
+    voicelessPhonemeCount,
+  };
 }
 
 function createGetVoicedPhonemeCount(voicedPhonemes) {
-  return phonemes => {
+  return (phonemes) => {
     return phonemes.reduce((accumulator, currentValue) => {
       const updatedValue = currentValue.slice(0, 2); //Remove possible "stress" markings (stress is indicated by a number in the 2 position (0-indexed)
-      if(voicedPhonemes.includes(updatedValue)) {
+      if (voicedPhonemes.includes(updatedValue)) {
         return accumulator + 1;
       }
       return accumulator;
-    }, 0)
-  }
+    }, 0);
+  };
 }
 const getVoicedPhonemeCount = createGetVoicedPhonemeCount(voicedPhonemes);
 
-const babyNamesWithVoicedPhonemesCount = getBabyNamesWithVoicedPhonemesCount(babyNames);
+const babyNamesWithVoicedPhonemesCount = getBabyNamesWithVoicedPhonemesCount(
+  babyNames
+);
 
-console.log(babyNamesWithVoicedPhonemesCount)
+const babyNamesWithPopularity = babyNamesWithVoicedPhonemesCount.map((bn) => {
+  const popularity = (() => {
+    const popData = usbabynames.byName[bn.name];
+    if (popData) {
+      return popData[popData.length - 1];
+    } else {
+      return null;
+    }
+  })();
+  return { ...bn, popularity: popularity };
+});
+
+console.log(babyNamesWithPopularity);
